a50f816ab685233d763e89364f796e9ca6728bc4
Hahn Stralle <84215792+hahn-stralle@users.noreply.github.com>
2021-05-27 19:24:41 +0900

    Add files via upload

--- convert_utvideo_hd.cmd
+++ convert_utvideo_hd.cmd
@@ -16,7 +16,7 @@ if not %1==return (
 ) else (
 	rem 入力動画ファイルをutvideoに変換して出力します。
 	mkdir %export_directory_path% 2>nul
-	ffmpeg -i %2 -an -r 24 -c:v utvideo %export_directory_path%%~n2%temporary_file_extension%
+	ffmpeg -i %2 -an -s 1280x720 -r 24 -c:v utvideo %export_directory_path%%~n2%temporary_file_extension%
 	cd %export_directory_path%
 	ren %~n2%temporary_file_extension% %~n2%export_file_extension%
 	cd %~dp0
